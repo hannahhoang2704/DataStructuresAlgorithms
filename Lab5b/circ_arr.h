25c2731c5dbf2d72b760c89035b9c5d75350a5fd
hannahhoang2704 <hanh.hoang@metropolia.fi>
2025-01-25 13:55:12 +0200

    fix lab5b

--- Lab5b/circ_arr.h
+++ Lab5b/circ_arr.h
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-#define MAXN 100000
+//#define MAXN 100000
 
 template <class Titem>
 class Queue {
 private:
-    Titem array[MAXN];
+    Titem *array;
     int first;
     int last;
     int number_of_items;
+    int capacity;
+    void expand();
 public:
     Queue();
+    ~Queue();
     bool enqueue(Titem item);
     bool dequeue(Titem &item);
     void print();
@@ -20,16 +23,38 @@ public:
 
 template <class Titem>
 Queue<Titem>::Queue() {
+    capacity = 10;
+    array = new Titem[capacity];
     first = 0;
     last = -1;
     number_of_items = 0;
 }
+
+template <class Titem>
+Queue<Titem>::~Queue(){
+    delete[] array;
+}
+
+template <class Titem>
+void Queue<Titem>::expand() {
+    int new_capacity = capacity*2;
+    Titem *new_arr = new Titem[new_capacity];
+    for(int i = 0; i < number_of_items; i++){
+        new_arr[i] = array[(first+i) %capacity];
+    }
+    delete[] array;
+    array = new_arr;
+    capacity = new_capacity;
+    first = 0;
+    last = number_of_items-1;
+}
+
 template <class Titem>
 bool Queue<Titem>::enqueue(Titem item) {
-    if (number_of_items >= MAXN)
-        return(false);
+    if (number_of_items >= capacity)
+        expand();
     else {
-        if (++last > MAXN - 1) last = 0;
+        last = (last+1)%capacity;
         array[last] = item;
         number_of_items++;
         return (true);
@@ -40,8 +65,8 @@ bool Queue<Titem>::dequeue(Titem &item) {
     if (number_of_items == 0)
         return(false);
     else {
-        item = array[first++];
-        if (first > MAXN - 1) first = 0;
+        item = array[first];
+        first = (first+1)%capacity;
         number_of_items--;
         return (true);
     }
@@ -49,9 +74,9 @@ bool Queue<Titem>::dequeue(Titem &item) {
 template <class Titem>
 void Queue<Titem>::print() {
     int i, n;
-    cout << "\nQueue now: \n\n";for (n = 1, i=first; n <= number_of_items ; n++) {cout << " " << array[i] << " ";i++;
-        if (i > MAXN-1)
-            i = 0;
+    cout << "\nQueue now: \n\n";
+    for (n = 1, i=first; n <= number_of_items ; n++) {
+        cout << " " << array[i] << " "; i++;
     }
     cout << endl << endl;
 }
