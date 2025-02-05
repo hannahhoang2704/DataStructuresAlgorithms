3d0e75b383161024c2773118ba2f6676a67a3f68
hannahhoang2704 <hanh.hoang@metropolia.fi>
2025-02-05 11:43:28 +0200

    add csv file writing

--- Lab12b/fft.cpp
+++ Lab12b/fft.cpp
@@ -8,6 +8,7 @@
 
 #include <complex>
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <vector>
 #include <cmath>
@@ -15,21 +16,25 @@
 
 #include "measure.h"
 #include "fft_func.h"
-
+#define MIN_N 2048
+//#define MAX_N 4096
+#define MAX_N 262144
 
 int main() {
     chrono::time_point<chrono::high_resolution_clock> start_program = chrono::high_resolution_clock::now();
     double mean_unopt, stdev_unopt, mean_unopt_br, stdev_unopt_br, mean_single, stdev_single, mean_single_br, \
     stdev_single_br, mean_parallel, stdev_parallel, mean_parallel_br, stdev_parallel_br;
-//    int N = 262144;
-//    FFTTestbed *p = new FFTTestbed(fft_parallel, N);
-//    p->measure(mean_unopt, stdev_unopt);
-//    delete p;
+    ofstream csvFile("/Users/HannahHoang/DataStructure/Lab12b/fft_measurements.csv");
+    if (!csvFile) {
+        cerr << "Error opening file for writing." << endl;
+        return 1;
+    }
+    csvFile << "N,Unopt,Unopt_stdev,Unopt_br,Unopt_br_stdev,Single,Single_br,Single_br_stdev,Parallel,Parallel_stdev,Parallel_br,Parallel_br_stdev" << endl;
     cout << setw(7) << right << " N "
          << setw(12) << right << "Unopt" << setw(17) << right << "Unopt_br"
          << setw(12) << right << "Single" << setw(16) << right << "Single_br"
          << setw(14) << right << "Parallel" << setw(14) << right << "Paral_br"<< endl;
-    for(int N = 2048; N <= 262144; N*=2){
+    for(int N = MIN_N; N <= MAX_N; N*=2){
         cout << setw(7) << right <<  N << ": ";
 
         FFTTestbed *unopt = new FFTTestbed(fft_unopt, N);
@@ -56,7 +61,15 @@ int main() {
         parallel_br->measure(mean_parallel_br, stdev_parallel_br);
         cout << setw(4) << right << (int)mean_parallel_br << "(+/- " << (int)stdev_parallel_br * 2 << ")ms";
         cout << endl;
+        csvFile << N << ","
+                << mean_unopt << "," << stdev_unopt * 2 << ","
+                << mean_unopt_br << "," << stdev_unopt_br * 2 << ","
+                << mean_single << ","
+                << mean_single_br << "," << stdev_single_br * 2 << ","
+                << mean_parallel << "," << stdev_parallel * 2 << ","
+                << mean_parallel_br << "," << stdev_parallel_br * 2 << endl;
     }
+    csvFile.close();
     chrono::time_point<chrono::high_resolution_clock> end_program = chrono::high_resolution_clock::now();
     auto duration_program = chrono::duration_cast<chrono::seconds> (end_program-start_program).count();
     cout << "Measurement took " << duration_program << "s" << endl;
